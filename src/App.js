f74799124dcfd6c6e37baf153cb5665b800b9538
spurgason <shanepurgason.98@gmail.com>
2022-03-09 21:42:29 -0500

    added login and login validation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from "react-chat-engine";
 
 import ChatFeed from "./components/ChatFeed"
+import LoginForm from "./components/LoginForm";
 
 import "./App.css";
 
 const App = () => {
+
+  if(!localStorage.getItem('username')) return <LoginForm />
   return (
     <ChatEngine
       height="100vh"
       projectID="479a9076-012e-4c70-bdb9-9eb1863aa32d"
-      userName="Coach"
-      userSecret="123123"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
